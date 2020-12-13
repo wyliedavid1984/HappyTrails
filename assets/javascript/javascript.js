48e9519a50a2011925c0de3d02579137c44c0bd8
wylie <wyliedavid1984@gmail.com>
2020-12-13 14:40:29 -0500

    added some error handling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,35 +8,51 @@ $(document).ready(function () {
     var map;
     var trailLocation = [];
     var weatherKey = "34af04e7087783be92496c2a33100782";
+  
+    // setting button clicks to specific functions
+    $("#gps").on("click", gps);
 
     // this is for if the user doesn't enter anything in the fields.
+    $("#zipCode").on("click", function (event) {
+        event.preventDefault();
 
-    $(document).on("click", "#gps", "#zipCode", function () {
-
-        if (!$("#zip")) {
-            $("#userCity").addClass("hidden");
-            $("#error").removeClass("hidden");
-            setTimeout(function () {
-
-                location.reload();
-            }, 8000);
-        } else if ($("#city") === "") {
+        if ($("#zip").val() === "") {
+            console.log("hizip")
             $("#userCity").addClass("hidden");
             $("#error").removeClass("hidden");
             setTimeout(function () {
                 location.reload();
-            }, 8000);
+            }, 1000);
         } else {
-            $("#gps").on("click", gps);
-            $("#zipCode").on("click", zipCode);
+            console.log('hizipno')
+            // hiding previous div and show the next
+            $("#userCity").addClass("hidden");
+            $("#hikingParameters").removeClass("hidden");
+
+            noGps();
         }
     });
 
+    // this is for if the user doesn't enter anything in the fields.
+    $("#no-gps").on("click", function (event) {
+        event.preventDefault();
+        if ($("#city").val() === "") {
+            console.log("hicity")
+            $("#userCity").addClass("hidden");
+            $("#error").removeClass("hidden");
+            setTimeout(function () {
+                location.reload();
+            }, 1000);
+        } else {
+            // hiding previous div and show the next
+            console.log("hicityno")
+            $("#userCity").addClass("hidden");
+            $("#hikingParameters").removeClass("hidden");
 
-    // setting button clicks to specific functions
-    $("#no-gps").on("click", noGps);
-
-
+            zipCode();
+        }
+    });
+    
     // uses the geolocation built in func to get lat and long coordinates
     function getLocation() {
         // Make sure browser supports this feature
@@ -94,8 +110,8 @@ $(document).ready(function () {
     }
 
     // another user input function takes in zip code.
-    function zipCode(e) {
-        e.preventDefault();
+    function zipCode() {
+       
 
         // user input
         var zip = $("#zip").val();
@@ -108,8 +124,8 @@ $(document).ready(function () {
     }
 
     // function that takes in parameter if the user doesn't want to share location
-    function noGps(e) {
-        e.preventDefault();
+    function noGps() {
+       
 
         // we are getting the value of the city from the user.
         var city = $("#city").val();
@@ -122,10 +138,7 @@ $(document).ready(function () {
 
     // this function will get lat and long to put together a hiking URL
     function getLocal(URL) {
-        // hiding previous div and show the next
-        $("#userCity").addClass("hidden");
-        $("#hikingParameters").removeClass("hidden");
-
+        
         // first ajax to get the city's lat and lon
         $(document).on("click", "#parameters", function (e) {
 
