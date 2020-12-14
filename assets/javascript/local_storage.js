f8e190e395a0ae5a0b1520f43ef79b4ac13d1929
wylie <wyliedavid1984@gmail.com>
2020-12-14 16:31:54 -0500

    more rearranging js

--- assets/javascript/local_storage.js
+++ assets/javascript/local_storage.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
   console.log("ready!");
 
-
-    var allFavorites = [];
+var allFavorites = [];
+  
+    
     console.log(allFavorites);
 
     //Set up a function to list the user's favorite hikes that are in localStorage
 
     //Convert the string into a JSON object
     allFavorites = JSON.parse(localStorage.getItem("savedTrails"));
+    
 $("#myFavorites").on("click", function () {
     if (allFavorites == null) {
       allFavorites = [];
@@ -26,6 +28,9 @@ $("#myFavorites").on("click", function () {
     // console.log(allFavorites);
   });
 
+if (allFavorites === null){
+    allFavorites = [];
+  }
 
   //Setting up the functions to save favorite trails to local storage
   $("#faveHike0").on("click", function () {
